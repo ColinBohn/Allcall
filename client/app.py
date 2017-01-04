37d3a4fb4fe3dcac1f9c5c8483d27f4b3b472d22
ColinBohn <colinbohn@gmail.com>
2017-01-04 15:31:02 -0800

    Additional client node changes
    Authentication, Jsonify, Dockerified

--- client/app.py
+++ client/app.py
@@ -1,14 +1,16 @@
 import threading
 import simpleaudio as sa
 import logging
-from flask import Flask, Response
+from flask import Flask, Response, jsonify, request
 from time import sleep
-from os import path
+from os import path, environ
+from functools import wraps
+
 
 playing = None
 thread = None
 stop_flag = False
-    
+
 app = Flask(__name__)
 handler = logging.FileHandler('app.log')
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
@@ -16,71 +18,77 @@ handler.setFormatter(formatter)
 app.logger.setLevel(logging.INFO)
 app.logger.addHandler(handler)
 
+def authenticated(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        header = request.headers.get('X-AllCall-Key')
+        key = environ.get('ALLCALL_NODE_KEY')
+        if not key or not header or not header == key:
+            return jsonify({"status" : "error",
+                    "response" : "Not authenticated properly"}), 401
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 @app.route('/ping')
 def ping():
-    js = '{"status":"success", "response":"pong"}'
-    return Response(js, status=200, mimetype='application/json')
+     return jsonify({"status" : "success", "response" : "pong"})
     
 @app.route('/status')
+@authenticated
 def status():
     if not thread:
-        js = '{"status":"success", "response":"Idle"}'
+        return jsonify({"status" : "success", "response" : "Idle"})
     else:
         global playing
-        js = '{"status":"success", "response":"Playing '+playing+'"}'
-
-    return Response(js, status=200, mimetype='application/json')
-
+        return jsonify({"status" : "success",
+                "response" : "Playing " + playing })
 
 @app.route('/start/')
 @app.route('/start/<alert>')
 @app.route('/start/<alert>/<int:delay>')
+@authenticated
 def start(alert = None, delay = 0):
     global thread
     global stop_flag
     if not alert:
         app.logger.warning("Received /start call with no alert specified")
-        js = '{"status":"error", "response":"Missing alert and/or delay input"}'
-        res = Response(js, status=400, mimetype='application/json')
+        return jsonify({"status" : "error",
+                "response" : "Missing alert and/or delay input"}), 400
         
     elif not path.isfile("audio/" + alert + ".wav"):
         app.logger.warning("Received /start call with invalid alert name")
-        js = '{"status":"error", "response":"No file exists with that name"}'
-        res = Response(js, status=400, mimetype='application/json')
+        return jsonify({"status" : "error",
+                "response" : "No file exists with that name"}), 400
         
     else:   
         if not thread:
-            js = '{"status":"success", "response":"Started playback of audio"}'
+            json = jsonify({"status" : "success",
+                    "response" : "Started playback of audio"})
         
         else:
             app.logger.info("Stopping playback to switch to new audio file")
             stopAudio()
-            js = '{"status":"success", "response":"Switched audio playback"}'
+            json = jsonify({"status" : "success",
+                    "response" : "Switched audio playback"})
         
-        res = Response(js, status=202, mimetype='application/json')
         thread = threading.Thread(target=loopAudio, args=(alert, delay,
                 lambda: stop_flag))
         app.logger.info("Starting playback of file " + alert)
         thread.start()
-
-    return res
+        return json
 
 
 @app.route('/stop')
+@authenticated
 def stop():
-    result = stopAudio()
-    if result == 202:
-        js = '{"status":"success", "response":"Playback requested to stop"}'
-        res = Response(js, status=202, mimetype='application/json')
-    elif result == 200:
-        js = '{"status":"success", "response":"No audio currently being played"}'
-        res = Response(js, status=200, mimetype='application/json')
+    stopped = stopAudio()
+    if stopped:
+        return jsonify({"status" : "success",
+                "response" : "Playback requested to stop"})
     else:
-        js = '{"status":"error", "response":"Unknown response from stopAudio"}'
-        res = Response(js, status=500, mimetype='application/json')
-    return res
+        return jsonify({"status" : "success",
+                "response" : "No audio currently being played"})
 
     
 def stopAudio():
@@ -95,10 +103,10 @@ def stopAudio():
         playing = None
         stop_flag = False
         app.logger.info("Successfully stopped audio playback")
-        return 202
+        return True
     else:
         app.logger.info("No audio was playing")
-        return 200
+        return False
 
 
 def loopAudio(alert, loop_delay, stop):
@@ -117,8 +125,6 @@ def loopAudio(alert, loop_delay, stop):
             sleep(1)
             if stop():
                 return # Uses return to break out of outer loop
-                
-               
-def authenticated(token):
-    # TODO: Add authentication method
-    return True
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
